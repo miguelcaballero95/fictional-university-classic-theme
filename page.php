9595d30937c66ad4aa581ebf4b89e1ae41c558c3
Miguel Caballero <mc.caballero95@gmail.com>
2025-01-25 12:06:07 -0500

    adding parent and child pages navigation

--- page.php
+++ page.php
@@ -15,19 +15,36 @@ while (have_posts()) {
     </div>
 
     <div class="container container--narrow page-section">
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p>
-                <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
-            </p>
-        </div>
+        <?php
+        $parent_page_id = wp_get_post_parent_id(get_the_ID());
+        if ($parent_page_id): ?>
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                    <a class="metabox__blog-home-link" href="<?php echo get_the_permalink($parent_page_id); ?>">
+                        <i class="fa fa-home" aria-hidden="true"></i>
+                        Back to <?php echo get_the_title($parent_page_id); ?>
+                    </a>
+                    <span class="metabox__main"><?php the_title(); ?></span>
+                </p>
+            </div>
+        <?php endif; ?>
 
-        <!-- <div class="page-links">
-            <h2 class="page-links__title"><a href="#">About Us</a></h2>
-            <ul class="min-list">
-                <li class="current_page_item"><a href="#">Our History</a></li>
-                <li><a href="#">Our Goals</a></li>
-            </ul>
-        </div> -->
+        <?php
+        $child_pages = get_pages(['child_of' => get_the_ID()]);
+        if ($parent_page_id || !empty($child_pages)): ?>
+            <div class="page-links">
+                <h2 class="page-links__title"><a href="<?php echo get_the_permalink($parent_page_id); ?>"><?php echo get_the_title($parent_page_id); ?></a></h2>
+                <ul class="min-list">
+                    <?php
+                    $child_of = $parent_page_id !== 0 ? $parent_page_id : get_the_ID();
+                    wp_list_pages([
+                        'title_li'  => null,
+                        'child_of'  => $child_of,
+                        'short_column'  => 'menu_order'
+                    ]); ?>
+                </ul>
+            </div>
+        <?php endif; ?>
 
         <div class="generic-content">
             <?php the_content(); ?>
